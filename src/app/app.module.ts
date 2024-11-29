d78c28d18d782caa965dc3cffeebeab5f5155bd0
manbarmohamed <johndoe@example.com>
2024-11-29 14:15:25 +0100

    create list-products and create-products components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ListProductsComponent } from './product/list-products/list-products.component';
+import { CreateProductsComponent } from './product/create-products/create-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListProductsComponent,
+    CreateProductsComponent
   ],
   imports: [
     BrowserModule,
