9f823f5da69d7cba02da313690dc35bbf08f5409
manbarmohamed <johndoe@example.com>
2024-11-29 14:19:18 +0100

    add routing in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:'/products/list', pathMatch:'full'
+  },
+  {
+    path:'products',
+    loadChildren: ()=> import('./product/product.module').then(m => m.ProductModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
