5ae9396ae8126ea0c4626f22fc6f4a761fb4bbcd
manbarmohamed <johndoe@example.com>
2024-11-29 14:21:01 +0100

    create routes in product-routing.module

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { CreateProductsComponent } from './create-products/create-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'list', component:ListProductsComponent},
+  {path:'create', component:CreateProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
